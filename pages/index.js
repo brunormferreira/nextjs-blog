70e60469d2858bce15385d63ad63457be6eb4ac4
Bruno <brunormferreira@hotmail.com>
2020-09-01 12:53:08 -0300

    add(feat): start active next blog

--- pages/index.js
+++ pages/index.js
@@ -8,31 +8,43 @@ import { getSortedPostsData } from '../lib/posts'
 
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home ({ allPostsData }) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! I'm a jr frontend developer and I use technologies to make peoples happy!</p>
+        <p>
+          Hello! I'm a jr frontend developer and I use technologies to make
+          peoples happy!
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href="/posts/[id]" as={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
+      <footer>
+        <ul className={utilStyles.list}>
+          <li className={utilStyles.listItem}>
+            <a href="https://brunormferreira.github.io" target="_blank">
+              Site
+            </a>
+          </li>
+        </ul>
+      </footer>
     </Layout>
   )
 }
@@ -41,7 +53,7 @@ export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
---
+++ posts/why-use-good-structure.md
@@ -0,0 +1,15 @@
+---
+title: 'Why to Use a Good Project Structure?'
+date: '2020-09-01'
+---
+
+Sometimes we had an project to work on, and make adjusts, or improove some features, its really relative what we should do on everyday, so the title says everyting.
+
+A good structure to your projects prevents a lot of problems, and make this more simple to admin, make adjusts and any other thing. On frontend, and backend, we need to be solid when start creating a structure, we need to think how scalable the projects it is? How are your team? Have a lot of thing that include for the project have success. But, technically speaking, the structure is the heart of a project.
+
+Think in a **good place**, good structure, easy to find the problems, easy to find your components, architecture its so important.
+
+Now, think in a **bad place**, where the files are all together, no separations, no scalable, so its hard to make peoples understand and make good adjusts on that.  
+Its very simple, _think_, _read_, _sketch_, make your structures better for a better codes and architectures.
+
+Thanks for reading, see you soon!
